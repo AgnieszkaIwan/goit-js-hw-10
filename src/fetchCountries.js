6533da9f40f835adb0e376f033232832233c8214
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-05-05 23:24:33 +0200

    .

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,16 +3,14 @@ const API_URL = 'https://restcountries.com/v3.1';
 export const fetchCountries = name => {
   return fetch(
     `${API_URL}/name/${name}?fields=name,capital,population,flags,languages`
-  ).then(res => res.json());
+  )
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Country not found');
+      }
+      return res.json();
+    })
+    .catch(error => {
+      throw error;
+    });
 };
-//       .then(res => {
-//         if (!res.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-//         return res.json();
-//       })
-//       .catch(error => {
-//         console.error('Error:', error);
-//       })
-//   );
-// };
