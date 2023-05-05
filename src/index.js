05945fbde7b9097fa6c8af7204d5170949a43c89
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-05-06 00:15:35 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries.js';
-import { debounce } from 'lodash.debounce';
+import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
